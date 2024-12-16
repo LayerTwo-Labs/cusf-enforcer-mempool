645453d65ec27d762cfea8bb67f5c13d76058a8b
Ash Manning <ash@layertwolabs.com>
2024-12-16 21:21:19 +0800

    Generalize CusfEnforcer trait

--- lib/server.rs
+++ lib/server.rs
@@ -1,21 +1,31 @@
+use std::convert::Infallible;
+
 use async_trait::async_trait;
 use bip300301::client::{
     BlockTemplate, BlockTemplateRequest, BlockTemplateTransaction,
     CoinbaseTxnOrValue, NetworkInfo,
 };
-use bitcoin::{amount::CheckedSum, Transaction};
+use bitcoin::{
+    amount::CheckedSum, hashes::Hash as _, merkle_tree, script::PushBytesBuf,
+    Amount, Block, Network, ScriptBuf, Transaction, TxOut, Txid,
+    WitnessMerkleNode, Wtxid,
+};
 use chrono::Utc;
+use educe::Educe;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc, types::ErrorCode};
 use thiserror::Error;
 
-use crate::{cusf_enforcer::CusfEnforcer, mempool::MempoolSync};
+use crate::{
+    cusf_block_producer::{self, CusfBlockProducer, InitialBlockTemplate},
+    mempool::MempoolSync,
+};
 
-#[rpc(server)]
+#[rpc(client, server)]
 pub trait Rpc {
     #[method(name = "getblocktemplate")]
     async fn get_block_template(
         &self,
-        _request: BlockTemplateRequest,
+        request: BlockTemplateRequest,
     ) -> RpcResult<BlockTemplate>;
 }
 
@@ -26,14 +36,18 @@ pub enum CreateServerError {
 }
 
 pub struct Server<Enforcer> {
+    coinbase_spk: ScriptBuf,
     mempool: MempoolSync<Enforcer>,
+    network: Network,
     network_info: NetworkInfo,
     sample_block_template: BlockTemplate,
 }
 
 impl<Enforcer> Server<Enforcer> {
     pub fn new(
+        coinbase_spk: ScriptBuf,
         mempool: MempoolSync<Enforcer>,
+        network: Network,
         network_info: NetworkInfo,
         sample_block_template: BlockTemplate,
     ) -> Result<Self, CreateServerError> {
@@ -44,7 +58,9 @@ impl<Enforcer> Server<Enforcer> {
             return Err(CreateServerError::SampleBlockTemplate);
         };
         Ok(Self {
+            coinbase_spk,
             mempool,
+            network,
             network_info,
             sample_block_template,
         })
@@ -73,10 +89,285 @@ where
     )
 }
 
-#[async_trait]
-impl<Enforcer> RpcServer for Server<Enforcer>
+/// Compute the block reward for the specified height
+fn get_block_reward(height: u32, fees: Amount, network: Network) -> Amount {
+    let subsidy_sats = 50 * Amount::ONE_BTC.to_sat();
+    let subsidy_halving_interval = match network {
+        Network::Regtest => 150,
+        _ => bitcoin::constants::SUBSIDY_HALVING_INTERVAL,
+    };
+    let halvings = height / subsidy_halving_interval;
+    if halvings >= 64 {
+        fees
+    } else {
+        fees + Amount::from_sat(subsidy_sats >> halvings)
+    }
+}
+
+const WITNESS_RESERVED_VALUE: [u8; 32] = [0; 32];
+
+/// Add witness commitment output to the coinbase tx, and return a copy of the
+/// witness commitment spk.
+/// The coinbase tx should not include the witness commitment txout.
+/// Signet challenge should be `Some` for signets, and `None` otherwise.
+fn add_witness_commitment_output(
+    coinbase_tx: &mut Transaction,
+    transactions: &[BlockTemplateTransaction],
+) -> ScriptBuf {
+    let witness_root = {
+        let hashes = std::iter::once(Wtxid::all_zeros().to_raw_hash())
+            .chain(transactions.iter().map(|tx| tx.hash.to_raw_hash()));
+        merkle_tree::calculate_root(hashes)
+            .map(WitnessMerkleNode::from_raw_hash)
+            .unwrap()
+    };
+    let witness_commitment = Block::compute_witness_commitment(
+        &witness_root,
+        &WITNESS_RESERVED_VALUE,
+    );
+    // https://github.com/bitcoin/bips/blob/master/bip-0141.mediawiki#commitment-structure
+    let witness_commitment_spk = {
+        const WITNESS_COMMITMENT_HEADER: [u8; 4] = [0xaa, 0x21, 0xa9, 0xed];
+        let mut push_bytes = PushBytesBuf::from(WITNESS_COMMITMENT_HEADER);
+        let () = push_bytes
+            .extend_from_slice(witness_commitment.as_byte_array())
+            .unwrap();
+        ScriptBuf::new_op_return(push_bytes)
+    };
+    coinbase_tx.output.push(TxOut {
+        value: Amount::ZERO,
+        script_pubkey: witness_commitment_spk.clone(),
+    });
+    witness_commitment_spk
+}
+
+#[derive(Debug, Error)]
+enum FinalizeCoinbaseTxError {
+    #[error("Coinbase reward underflow")]
+    CoinbaseRewardUnderflow,
+    #[error("Fee overflow")]
+    FeeOverflow,
+    #[error(
+        "Negative tx fee for tx `{txid}` at index `{tx_index}`: `{}`",
+        .fee.display_dynamic()
+    )]
+    NegativeTxFee {
+        txid: Txid,
+        tx_index: usize,
+        fee: bitcoin::SignedAmount,
+    },
+}
+
+/// Finalize coinbase tx.
+/// The witness commitment output in the coinbase is not added on signets.
+/// Returns the coinbase tx, and (on networks other than signets) the witness
+/// commitment spk.
+fn finalize_coinbase_tx(
+    coinbase_spk: ScriptBuf,
+    block_height: u32,
+    network: Network,
+    mut coinbase_txouts: Vec<TxOut>,
+    transactions: &[BlockTemplateTransaction],
+) -> Result<(Transaction, Option<ScriptBuf>), FinalizeCoinbaseTxError> {
+    let bip34_height_script = bitcoin::blockdata::script::Builder::new()
+        .push_int(block_height as i64)
+        .push_opcode(bitcoin::opcodes::OP_0)
+        .into_script();
+    let fees = transactions.iter().enumerate().try_fold(
+        Amount::ZERO,
+        |fees_acc, (tx_index, tx)| {
+            let fee = tx.fee.to_unsigned().map_err(|_| {
+                FinalizeCoinbaseTxError::NegativeTxFee {
+                    txid: tx.txid,
+                    tx_index,
+                    fee: tx.fee,
+                }
+            })?;
+            fees_acc
+                .checked_add(fee)
+                .ok_or(FinalizeCoinbaseTxError::FeeOverflow)
+        },
+    )?;
+    let block_reward = get_block_reward(block_height, fees, network);
+    // Remaining block reward value to add to coinbase txouts
+    let coinbase_reward = coinbase_txouts.iter().try_fold(
+        block_reward,
+        |reward_acc, txout| {
+            reward_acc
+                .checked_sub(txout.value)
+                .ok_or(FinalizeCoinbaseTxError::CoinbaseRewardUnderflow)
+        },
+    )?;
+    tracing::debug!(
+        block_reward = %block_reward.display_dynamic(),
+        coinbase_reward = %coinbase_reward.display_dynamic(),
+        fees = %fees.display_dynamic(),
+    );
+    if coinbase_reward > Amount::ZERO {
+        coinbase_txouts.push(TxOut {
+            value: coinbase_reward,
+            script_pubkey: coinbase_spk,
+        })
+    }
+    let mut coinbase_tx = Transaction {
+        version: bitcoin::transaction::Version::TWO,
+        lock_time: bitcoin::absolute::LockTime::ZERO,
+        input: vec![bitcoin::TxIn {
+            previous_output: bitcoin::OutPoint {
+                txid: Txid::all_zeros(),
+                vout: 0xFFFF_FFFF,
+            },
+            sequence: bitcoin::Sequence::MAX,
+            witness: bitcoin::Witness::from_slice(&[WITNESS_RESERVED_VALUE]),
+            script_sig: bip34_height_script,
+        }],
+        output: coinbase_txouts,
+    };
+    let witness_commitment_spk = match network {
+        Network::Signet => None,
+        _ => Some(add_witness_commitment_output(
+            &mut coinbase_tx,
+            transactions,
+        )),
+    };
+    Ok((coinbase_tx, witness_commitment_spk))
+}
+
+#[derive(Educe)]
+#[educe(Debug(bound()))]
+#[derive(Error)]
+enum BuildBlockError<BP>
 where
-    Enforcer: CusfEnforcer + Send + Sync + 'static,
+    BP: CusfBlockProducer,
+{
+    #[error(transparent)]
+    FinalizeCoinbaseTx(#[from] FinalizeCoinbaseTxError),
+    #[error(transparent)]
+    InitialBlockTemplate(BP::InitialBlockTemplateError),
+    #[error(transparent)]
+    MempoolInsert(#[from] crate::mempool::MempoolInsertError),
+    #[error(transparent)]
+    MempoolRemove(#[from] crate::mempool::MempoolRemoveError),
+    #[error(transparent)]
+    SuffixTxs(BP::SuffixTxsError),
+}
+
+// select block txs, and coinbase txouts if coinbasetxn is set
+fn block_txs<const COINBASE_TXN: bool, BP>(block_producer: &BP, mempool: &crate::mempool::Mempool)
+    -> Result<
+            (<typewit::const_marker::Bool<COINBASE_TXN> as cusf_block_producer::CoinbaseTxn>::CoinbaseTxouts,
+             Vec<BlockTemplateTransaction>),
+            BuildBlockError<BP>
+        >
+    where BP: CusfBlockProducer,
+    typewit::const_marker::Bool<COINBASE_TXN>: cusf_block_producer::CoinbaseTxn
+     {
+    let mut initial_block_template =
+        InitialBlockTemplate::<COINBASE_TXN>::default();
+    tracing::debug!("Generating initial block template");
+    initial_block_template = block_producer
+        .initial_block_template(
+            typewit::MakeTypeWitness::MAKE,
+            initial_block_template,
+        )
+        .map_err(BuildBlockError::InitialBlockTemplate)?;
+    let mut mempool = mempool.clone();
+    for (tx, fee) in initial_block_template.prefix_txs.iter().cloned() {
+        let _txinfo: Option<_> = mempool.insert(tx, fee.to_sat())?;
+    }
+    // depends field must be set later
+    let mut res_txs: Vec<_> = {
+        initial_block_template
+            .prefix_txs
+            .iter()
+            .map(|(tx, fee)| BlockTemplateTransaction {
+                data: bitcoin::consensus::serialize(tx),
+                txid: tx.compute_txid(),
+                hash: tx.compute_wtxid(),
+                depends: Vec::new(),
+                fee: (*fee).try_into().unwrap(),
+                sigops: None,
+                weight: tx.weight().to_wu(),
+            })
+            .collect()
+    };
+    {
+        // Remove prefix txs
+        let prefix_txids: std::collections::HashSet<Txid> =
+            initial_block_template
+                .prefix_txs
+                .iter()
+                .map(|(tx, _fee)| tx.compute_txid())
+                .collect();
+        let _removed_txs = mempool
+            .try_filter(false, |tx, _| {
+                let txid = tx.compute_txid();
+                Result::<_, Infallible>::Ok(!prefix_txids.contains(&txid))
+            })
+            .map_err(|err| match err {
+                either::Either::Left(err) => err,
+            })?;
+        let _removed_txs = mempool
+            .try_filter(true, |tx, _| {
+                let txid = tx.compute_txid();
+                Result::<_, Infallible>::Ok(
+                    !initial_block_template.exclude_mempool_txs.contains(&txid),
+                )
+            })
+            .map_err(|err| match err {
+                either::Either::Left(err) => err,
+            })?;
+    }
+    let mempool_txs = mempool.propose_txs()?;
+    initial_block_template
+        .prefix_txs
+        .extend(mempool_txs.iter().map(|tx| {
+            let fee = tx.fee.unsigned_abs();
+            let tx = bitcoin::consensus::deserialize(&tx.data).unwrap();
+            (tx, fee)
+        }));
+    tracing::debug!("Adding suffix txs");
+    let suffix_txs = block_producer
+        .suffix_txs(typewit::MakeTypeWitness::MAKE, &initial_block_template)
+        .map_err(BuildBlockError::SuffixTxs)?;
+    res_txs.extend(mempool_txs);
+    res_txs.extend(suffix_txs.iter().map(|(tx, fee)| {
+        BlockTemplateTransaction {
+            data: bitcoin::consensus::serialize(tx),
+            txid: tx.compute_txid(),
+            hash: tx.compute_wtxid(),
+            depends: Vec::new(),
+            fee: (*fee).try_into().unwrap(),
+            sigops: None,
+            weight: tx.weight().to_wu(),
+        }
+    }));
+    // Fill depends
+    {
+        let mut tx_indexes = std::collections::HashMap::new();
+        for (idx, tx) in res_txs.iter_mut().enumerate() {
+            tx_indexes.insert(tx.txid, idx as u32);
+            let tx_inputs =
+                bitcoin::consensus::deserialize::<Transaction>(&tx.data)
+                    .unwrap()
+                    .input;
+            for txin in tx_inputs {
+                if let Some(tx_idx) = tx_indexes.get(&txin.previous_output.txid)
+                {
+                    tx.depends.push(*tx_idx)
+                }
+            }
+            tx.depends.sort();
+            tx.depends.dedup();
+        }
+    }
+    Ok((initial_block_template.coinbase_txouts, res_txs))
+}
+
+#[async_trait]
+impl<BP> RpcServer for Server<BP>
+where
+    BP: CusfBlockProducer + Send + Sync + 'static,
 {
     async fn get_block_template(
         &self,
@@ -91,52 +382,71 @@ where
             ref version_bits_available,
             version_bits_required,
             ref coinbase_aux,
-            // FIXME: compute this
             ref coinbase_txn_or_value,
             ref mutable,
             sigop_limit,
             size_limit,
             weight_limit,
+            ref signet_challenge,
             ..
         } = self.sample_block_template;
-
-        // Some(_) iff client has indicated support for coinbasetxn
-        let mut coinbase_txn: Option<Transaction> =
-            if request.capabilities.contains("coinbasetxn") {
-                Some(Transaction {
-                    version: bitcoin::transaction::Version::TWO,
-                    lock_time: bitcoin::absolute::LockTime::ZERO,
-                    input: Vec::new(),
-                    output: Vec::new(),
-                })
-            } else {
-                None
-            };
+        let current_time_adjusted =
+            (now.timestamp() + self.network_info.time_offset_s) as u64;
         let (
             target,
             prev_blockhash,
             tip_block_mediantime,
             tip_block_height,
-            transactions,
+            coinbase_txn,
+            block_txs,
+            default_witness_commitment,
         ) = self
             .mempool
             .with(|mempool, enforcer| {
                 let tip_block = mempool.tip();
-                if let Some(coinbase_txn) = coinbase_txn.as_mut() {
-                    coinbase_txn.output = enforcer
-                        .modify_coinbase(Vec::new())
-                        .map_err(either::Either::Left)?
-                };
+                let (coinbase_txn, block_txs, default_witness_commitment) =
+                    if request.capabilities.contains("coinbasetxn") {
+                        tracing::debug!("Filling block txs");
+                        let (coinbase_txouts, block_txs) =
+                            block_txs::<true, _>(enforcer, mempool)?;
+                        tracing::debug!("Finalizing coinbase txn");
+                        let (coinbase_tx, witness_commitment_spk) =
+                            finalize_coinbase_tx(
+                                self.coinbase_spk.clone(),
+                                tip_block.height + 1,
+                                self.network,
+                                coinbase_txouts,
+                                &block_txs,
+                            )?;
+                        let default_witness_commitment =
+                            witness_commitment_spk.map(|spk| spk.to_bytes());
+                        (
+                            Some(coinbase_tx),
+                            block_txs,
+                            default_witness_commitment,
+                        )
+                    } else {
+                        let ((), block_txs) =
+                            block_txs::<false, _>(enforcer, mempool)?;
+                        (None, block_txs, None)
+                    };
                 Ok((
                     mempool.next_target(),
                     tip_block.hash,
                     tip_block.mediantime,
                     tip_block.height,
-                    mempool.propose_txs().map_err(either::Either::Right)?,
+                    coinbase_txn,
+                    block_txs,
+                    default_witness_commitment,
                 ))
             })
             .await
-            .map_err(|err: either::Either<_, _>| {
+            .ok_or_else(|| {
+                let err = anyhow::anyhow!("Mempool unavailable");
+                let err = log_error(err);
+                internal_error(err)
+            })?
+            .map_err(|err: BuildBlockError<_>| {
                 let err = log_error(err);
                 internal_error(err)
             })?;
@@ -157,8 +467,8 @@ where
                 txid: coinbase_txn.compute_txid(),
                 hash: coinbase_txn.compute_wtxid(),
                 depends: Vec::new(),
-                fee: -(fee.to_sat() as i64),
-                sigops: Some(coinbase_txn.total_sigop_cost(|_| None) as u64),
+                fee: -bitcoin::SignedAmount::try_from(fee).unwrap(),
+                sigops: None,
                 weight: coinbase_txn.weight().to_wu(),
                 data: bitcoin::consensus::serialize(&coinbase_txn),
             };
@@ -166,24 +476,18 @@ where
         } else {
             coinbase_txn_or_value.clone()
         };
-        let current_time_adjusted =
-            (now.timestamp() + self.network_info.time_offset_s) as u64;
         let mintime = std::cmp::max(
             tip_block_mediantime as u64 + 1,
             current_time_adjusted,
         );
-        let height = tip_block_height as u32 + 1;
+        let height = tip_block_height + 1;
         let res = BlockTemplate {
-            signet_challenge: self
-                .sample_block_template
-                .signet_challenge
-                .clone(),
             version,
             rules: rules.clone(),
             version_bits_available: version_bits_available.clone(),
             version_bits_required,
             prev_blockhash,
-            transactions,
+            transactions: block_txs,
             coinbase_aux: coinbase_aux.clone(),
             coinbase_txn_or_value,
             long_poll_id: None,
@@ -197,8 +501,8 @@ where
             current_time: current_time_adjusted,
             compact_target: target.to_compact_lossy(),
             height,
-            // FIXME: set this
-            default_witness_commitment: None,
+            default_witness_commitment,
+            signet_challenge: signet_challenge.clone(),
         };
         Ok(res)
     }
